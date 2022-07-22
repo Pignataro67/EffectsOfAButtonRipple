c4d648d907b94b181d271921d9dfd960d3e68469
Pignataro67 <Pignataro67@gmail.com>
2022-07-22 08:07:02 -0700

    adding JS

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
   button.addEventListener('click', function (e) {
-       
+    const x = e.clientX
+    const y = e.clientY
+
+    const buttonTop = e.target.offsetTop
+    const buttonLeft = e.target.offsetLeft
   })
 })
